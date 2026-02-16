59eab9c1ca24c366b14b11d6c863dc54084f7eb0
John Keiser <john@johnkeiser.com>
2026-02-16 14:49:12 -0800

    Text changes

--- src/App.svelte
+++ src/App.svelte
@@ -14,14 +14,17 @@
 
 <main>
   <h1>simdjson: Microparallel Parsing With Arithmetic</h1>
-  <p>simdjson takes a unique "microparallel" approach to parsing JSON. Instead of processing a document character by character, simdjson processes the document 64 bytes at a time, parsing all 64 bytes at once in parallel. To do this, it produces 64-bit <em>bitmasks</em> representing important features like quotes or backslashes, and then uses math and bitwise operations to figure out the answers it needs.</p>
+  <p>simdjson takes a unique "microparallel" approach to parsing JSON. Instead of processing a document character by character, simdjson processes the document 64 bytes at a time, producing 64-bit *bitmasks* to indicate features like quotes, arrays, and commas. It then does more complex "context-dependent" logic in a branch-free manner, figuring out thing like "is this character escaped" and "is this character in a string" using math and shift operations.</p>
   <div class="example-container">
-    <p class="example-text">For example, in order to parse strings, simdjson has to recognize escaped quotes like `\"`. To do <em>that,</em> it first creates a "backslash bitmask" with a 1 in any position that has a backslash (e.g. <code>"\"" -&gt; 0100</code>). Then, it shifts that mask by 1 for an escaped character bitmask (<code>0010</code>). Now when it wants to find "real quotes", it creates a quote bitmask, and uses bitwise operations <code>&amp;~</code> to subtract out the escaped quotes, correctly yielding <code>1001</code>.</p>
+    <p class="example-text">For example, in order to parse strings, simdjson has to recognize escaped quotes like `"quote: \""`. To do <em>that,</em> it first creates a "backslash bitmask" with a 1 in any position that has a backslash (e.g. <code>"\"" -&gt; 0100</code>). Then, it shifts that mask by 1 for an escaped character bitmask (<code>0010</code>). Now when it wants to find "real quotes", it creates a quote bitmask, and uses bitwise operations <code>&amp;~</code> to subtract out the escaped quotes, correctly yielding <code>1001</code>.</p>
     <div class="example-grid">
       <MaskGrid input={escapeExampleInput} rows={escapeExampleRows} blockSize={8} initialBlock={0} initialRow={3} />
     </div>
   </div>
-  <p>The following visualization shows this process in action! Hit the Step button to step through simdjson as it figures out which quotes are real and then uses that to figure out which characters are inside strings.</p>
+  <div>
+    When it's done with that, it uses a "carryless multiply" operation to create a mask showing which characters are inside strings. Then it finds numbers, booleans, and special characters like <code>&lbrace;</code> and <code>:</code>, and filters out the ones that are in strings using that mask. At which point, it's got all the real ones!
+  </div>
+  <p>The following visualization shows the full process in action! Hit the Play or Step button to step through simdjson as it figures out which quotes are real and then uses that to figure out which characters are inside strings.</p>
   <div class="figure">
     <MaskGrid input={inputString} rows={maskRows} />
   </div>
